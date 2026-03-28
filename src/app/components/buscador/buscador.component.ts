a1100d4f38b2c12ae247350a45945db66ddd4395
Alberto <alberto.velasco376@comunidadunir.net>
2026-03-28 20:10:27 +0100

    changes en el input del buscador

--- src/app/components/buscador/buscador.component.ts
+++ src/app/components/buscador/buscador.component.ts
@@ -8,53 +8,107 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { Producto } from '../../models/producto.model';
 import { CarritoService } from '../../services/carrito.service';
+import { ProductoService } from '../../services/producto.service';
 
 @Component({
   selector: 'app-buscador',
   standalone: true,
   imports: [
-    CommonModule, 
+    CommonModule,
     FormsModule, // <--- Verifica que esté aquí
-    MatCardModule, 
-    MatButtonModule, 
-    MatIconModule, 
-    MatFormFieldModule, 
-    MatInputModule
+    MatCardModule,
+    MatButtonModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatInputModule,
   ],
   templateUrl: './buscador.component.html',
-  styleUrl: './buscador.component.css'
+  styleUrl: './buscador.component.css',
 })
 export class BuscadorComponent implements OnInit {
   terminoBusqueda: string = '';
-  
+
   // Esta es la lista que se mostrará en el HTML
   productosFiltrados: Producto[] = [];
 
   // Tus datos reales
   listaProductos: Producto[] = [
-    { id: 1, nombre: 'Leche Entera 1L', precio: 0.95, supermercado: 'Mercadona', url: 'https://www.mercadona.es/', imagen: 'https://a2.soysuper.com/97fe0f4a203c6ce8811af3fba386c620.1500.0.0.0.wmark.b86ffd57.jpg' },
-    { id: 2, nombre: 'Leche Entera 1L', precio: 0.97, supermercado: 'Carrefour', url: 'https://www.carrefour.es/', imagen: 'https://static.carrefour.es/hd_510x_/img_pim_food/231395_00_1.jpg' },
-    { id: 3, nombre: 'Leche Entera 1L', precio: 0.97, supermercado: 'Dia', url: 'https://www.dia.es/', imagen: 'https://www.dia.es/product_images/608/608_ISO_0_ES.jpg' },
-    { id: 4, nombre: 'Huevos de gallinas camperas', precio: 2.20, supermercado: 'Mercadona', url: 'https://www.mercadona.es/', imagen: 'https://prod-mercadona.imgix.net/images/daf6a1cc96954346430dc055b82dff56.jpg?fit=crop&h=600&w=600' },
-    { id: 5, nombre: 'Huevos M-L ecológicos Carrefour Bio 6 ud.', precio: 2.85, supermercado: 'Carrefour', url: 'https://www.carrefour.es/', imagen: 'https://static.carrefour.es/hd_510x_/img_pim_food/864411_00_1.jpg' },
-    { id: 6, nombre: 'Huevos ecológicos grandes/medianos M/L Dia 6 unidades', precio: 2.95, supermercado: 'Dia', url: 'https://www.dia.es/', imagen: 'https://a0.soysuper.com/247a73a30ff014d5c141a4dcadb7cd4a.1500.0.0.0.wmark.eba64cc9.jpg' },
+    {
+      id: 1,
+      nombre: 'Leche Entera 1L',
+      precio: 0.95,
+      supermercado: 'Mercadona',
+      url: 'https://www.mercadona.es/',
+      imagen:
+        'https://a2.soysuper.com/97fe0f4a203c6ce8811af3fba386c620.1500.0.0.0.wmark.b86ffd57.jpg',
+    },
+    {
+      id: 2,
+      nombre: 'Leche Entera 1L',
+      precio: 0.97,
+      supermercado: 'Carrefour',
+      url: 'https://www.carrefour.es/',
+      imagen:
+        'https://static.carrefour.es/hd_510x_/img_pim_food/231395_00_1.jpg',
+    },
+    {
+      id: 3,
+      nombre: 'Leche Entera 1L',
+      precio: 0.97,
+      supermercado: 'Dia',
+      url: 'https://www.dia.es/',
+      imagen: 'https://www.dia.es/product_images/608/608_ISO_0_ES.jpg',
+    },
+    {
+      id: 4,
+      nombre: 'Huevos de gallinas camperas',
+      precio: 2.2,
+      supermercado: 'Mercadona',
+      url: 'https://www.mercadona.es/',
+      imagen:
+        'https://prod-mercadona.imgix.net/images/daf6a1cc96954346430dc055b82dff56.jpg?fit=crop&h=600&w=600',
+    },
+    {
+      id: 5,
+      nombre: 'Huevos M-L ecológicos Carrefour Bio 6 ud.',
+      precio: 2.85,
+      supermercado: 'Carrefour',
+      url: 'https://www.carrefour.es/',
+      imagen:
+        'https://static.carrefour.es/hd_510x_/img_pim_food/864411_00_1.jpg',
+    },
+    {
+      id: 6,
+      nombre: 'Huevos ecológicos grandes/medianos M/L Dia 6 unidades',
+      precio: 2.95,
+      supermercado: 'Dia',
+      url: 'https://www.dia.es/',
+      imagen:
+        'https://a0.soysuper.com/247a73a30ff014d5c141a4dcadb7cd4a.1500.0.0.0.wmark.eba64cc9.jpg',
+    },
   ];
 
-  constructor(private carritoService: CarritoService) {}
+  productos: any[] = [];
+
+  constructor(
+    private carritoService: CarritoService,
+    private productoService: ProductoService,
+  ) {}
 
   ngOnInit() {
     // Al empezar, mostramos todos los productos
-    this.productosFiltrados = this.listaProductos;
+    //this.productosFiltrados = this.listaProductos;
+
   }
 
   filtrar() {
     const termino = this.terminoBusqueda.toLowerCase().trim();
-    
+
     if (!termino) {
       this.productosFiltrados = this.listaProductos;
     } else {
-      this.productosFiltrados = this.listaProductos.filter(p => 
-        p.nombre.toLowerCase().includes(termino)
+      this.productosFiltrados = this.listaProductos.filter((p) =>
+        p.nombre.toLowerCase().includes(termino),
       );
     }
   }
@@ -63,4 +117,27 @@ export class BuscadorComponent implements OnInit {
     this.carritoService.agregar(p);
     alert('Añadido: ' + p.nombre);
   }
+
+  send(texto: string) {
+    this.productoService.scrapeProductos(texto).subscribe({
+      next: (res) => console.log(res),
+      error: (err) => console.error(err),
+      complete: () => {
+        this.cargarProductos();
+      }
+    });
+  }
+
+cargarProductos() {
+  this.productoService.getProductos().subscribe({
+    next: (res) => {
+      console.log(res);
+
+      if (res.code === 1) {
+        this.productos = res.data; // 👈 AQUÍ GUARDAS LOS PRODUCTOS
+      }
+    },
+    error: (err) => console.error(err)
+  });
+}
 }
--- src/app/services/producto.service.ts
+++ src/app/services/producto.service.ts
@@ -1,20 +1,23 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { Producto } from '../models/producto.model'; 
+import { Producto } from '../models/producto.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductoService {
 
-  
+
   private apiUrl = 'http://localhost:8080/api/productos';
 
   constructor(private http: HttpClient) { }
 
-  
-  getProductos(): Observable<Producto[]> {
-    return this.http.get<Producto[]>(this.apiUrl);
+
+  getProductos(): Observable<any> {
+      return this.http.get(`${this.apiUrl}/getAllProductos`);
   }
+  scrapeProductos(param: string): Observable<any> {
+  return this.http.post(`${this.apiUrl}/scrape`, { texto: param });
+}
 }
